24b061e32d5828b1ea8ee9efb94c80b4da4d98ea
EtienneBauer <etienne@john-bauer.com>
2025-03-02 16:03:23 -0500

    Savefig
    Saves figures to /figures folder now.

--- src/visualizing.py
+++ src/visualizing.py
@@ -16,4 +16,6 @@ def make_heatmap(data: pd.DataFrame) -> None:
     # In case I want to cut the heatmap in half
     sns.heatmap(data, annot = True, vmax = 1, vmin = 0, center = 0.5, fmt = ".2f", cmap = 'coolwarm', annot_kws = {"size":11})
     plt.title('Heatmap of Player 2 Win Percentages')
+    plt.savefig('figures/Win_Percentage_Heatmap.pdf', bbox_inches='tight')
+    plt.savefig('figures/Win_Percentage_Heatmap.png', bbox_inches='tight')
     plt.show()
